be88800abc652c17a5fb6b7c7463f23ea98107d1
masayuki.tanaka <masayuki.tanaka@diva.co.jp>
2021-04-08 01:28:26 +0900

    add rinna

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from transformers import T5Tokenizer, AutoModelForCausalLM
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello Flask, on Azure App Service for Linux"
+
+@app.route("/rinna")
+def rinna():
+    tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt2-medium")
+    model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
+    return "Hello Rinna"
