913140f49e5522f24f5a2c73f5e95801391e5c6d
masayuki.tanaka <masayuki.tanaka@diva.co.jp>
2021-04-08 02:30:44 +0900

    include rinna

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from transformers import T5Tokenizer, AutoModelForCausalLM
+from transformers import pipeline
+import re
 
 app = Flask(__name__)
+
+tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt2-medium")
+model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
+classifier = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
 @app.route("/")
 def hello():
     return "Hello Flask, on Azure App Service for Linux"
 
-@app.route("/rinna")
-def rinna():
-    tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt2-medium")
-    model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
-    return "Hello Rinna"
+@app.route("/rinna/<seed>")
+def rinna(seed=None):
+    result = classifier(seed,max_length=40)
+    resText = result[0]['generated_text']
+    m = re.search('^.+[.。$]',resText)
+    if m:
+        return m.group()
+    return resText
